d1dab93838da3aa59403016edd47b6a70505f0ef
matt <matt@serenade.ai>
2022-07-15 12:28:51 -0700

    add back python3

--- client/bin/dev.py
+++ client/bin/dev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import subprocess
@@ -7,7 +7,7 @@ import sys
 os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 
 if len(sys.argv) == 1:
-    subprocess.check_call("python ./bin/build.py", shell=True)
+    subprocess.check_call("python3 ./bin/build.py", shell=True)
 
 env = {**os.environ, **{"npm_config_arch": "x64"}}
 subprocess.check_call("npm run dev", env=env, shell=True)
