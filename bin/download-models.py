86bddbb1dd49b258d4f89006c381022dd1e6aeaf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-04 09:26:50 +0000

    Adding tarfile member sanitization to extractall()

--- bin/download-models.py
+++ bin/download-models.py
@@ -52,7 +52,26 @@ def main(url):
             serenade.packages.download(f"{url}/{path}/{model}.tar.gz", archive)
 
         with tarfile.open(archive, mode="r:gz") as f:
-            f.extractall(os.path.dirname(archive))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, os.path.dirname(archive))
 
         os.remove(archive)
 
